5c3e518d775afa318f09dbe43c5ef63c2275c234
Rbaxters <158490067+Rbaxters@users.noreply.github.com>
2025-02-06 11:33:09 -0500

    Update address.py

--- address.py
+++ address.py
@@ -1,5 +1,164 @@
 SA_Name = "Shahood Alvi"
-SA_Age = 17
-SA_Email = 
-SA_Grade
-SA_driversLicense
+SA_Age = 17.08
+SA_Email = "267741@burlcoschools.org"
+SA_Grade = 11
+SA_driversLicense = False
+
+RS_Name = "Ryan Stiles"
+RS_Age = 17.33
+RS_Email = "267947@burlcoschools.org"
+RS_Grade = 11
+RS_driversLicense = True
+
+MP_Name = "Marcos Paulino"
+MP_Age = 16.7
+MP_Email = "267895@burlcoschools.org"
+MP_Grade = 11
+MP_driversLicense = False
+
+AR_Name = "Aidan Rodriguez"
+AR_Age = 17.17
+AR_Email = "267922@burlcoschools.org"
+AR_Grade = 11
+AR_driversLicense = False
+
+GC_Name = "Gavin Carnivale"
+GC_Age = 17.04
+GC_Email = "267764@burlcoschools.org"
+GC_Grade = 11
+GC_driversLicense = True
+
+CH_Name = "Chris Hodson"
+CH_Age = 17.12
+CH_Email = "268359@burlcoschools.org"
+CH_Grade = 11
+CH_driversLicense = False
+
+NC_Name = "Nate Carn"
+NC_Age = 16.98
+NC_Email = "268021@burlcoschools.org"
+NC_Grade = 11
+NC_driversLicense = False
+
+LH_Name = "Leo Hart"
+LH_Age = 17.02
+LH_Email = "268095@burlcoschools.org"
+LH_Grade = 11
+LH_driversLicense = True
+
+MB_Name = "Mason Bush"
+MB_Age = 17.08
+MB_Email = "268262@burlcoschools.org"
+MB_Grade = 11
+MB_driversLicense = False
+
+MD_Name = "Muntag Diallo"
+MD_Age = 16.13
+MD_Email = "268154@burlcoschools.org"
+MD_Grade = 11
+MD_driversLicense = False
+
+JR_Name = "Jaiden Rankus"
+JR_Age = 16.44
+JR_Email = "268188@burlcoschools.org"
+JR_Grade = 11
+JR_driversLicense = False
+
+print("-----------------------------------------")
+print("| Name:", SA_Name, "                   |")
+print("| Age:", SA_Age,"                           |")
+print("| Grade:", SA_Grade,"                            |")
+print("| Drivers License:", SA_driversLicense,"               |")
+print("| Email:", SA_Email,"      |")
+print("-----------------------------------------")
+print()
+
+print("-----------------------------------------")
+print("| Name:", MP_Name ,"                 |")
+print("| Age:", MP_Age,"                            |")
+print("| Grade:", MP_Grade,"                            |")
+print("| Drivers License:", MP_driversLicense,"               |")
+print("| Email:", MP_Email,"      |")
+print("-----------------------------------------")
+print()
+
+print("-----------------------------------------")
+print("| Name:", RS_Name ,"                    |")
+print("| Age:", RS_Age,"                           |")
+print("| Grade:", RS_Grade,"                            |")
+print("| Drivers License:", RS_driversLicense,"                |")
+print("| Email:", RS_Email,"      |")
+print("-----------------------------------------")
+print()
+
+print("-----------------------------------------")
+print("| Name:", AR_Name ,"                |")
+print("| Age:", AR_Age,"                           |")
+print("| Grade:", AR_Grade,"                            |")
+print("| Drivers License:", AR_driversLicense,"               |")
+print("| Email:", AR_Email,"      |")
+print("-----------------------------------------")
+print()
+
+print("-----------------------------------------")
+print("| Name:", LH_Name ,"                       |")
+print("| Age:", LH_Age,"                           |")
+print("| Grade:", LH_Grade,"                            |")
+print("| Drivers License:", LH_driversLicense,"                |")
+print("| Email:", LH_Email,"      |")
+print("-----------------------------------------")
+print()
+
+print("-----------------------------------------")
+print("| Name:", CH_Name ,"                   |")
+print("| Age:", CH_Age,"                           |")
+print("| Grade:", CH_Grade,"                            |")
+print("| Drivers License:", CH_driversLicense,"               |")
+print("| Email:", CH_Email,"      |")
+print("-----------------------------------------")
+print()
+
+print("-----------------------------------------")
+print("| Name:", GC_Name ,"                |")
+print("| Age:", GC_Age,"                           |")
+print("| Grade:", GC_Grade,"                            |")
+print("| Drivers License:", GC_driversLicense,"                |")
+print("| Email:", GC_Email,"      |")
+print("-----------------------------------------")
+print()
+
+print("-----------------------------------------")
+print("| Name:", NC_Name ,"                      |")
+print("| Age:", NC_Age,"                           |")
+print("| Grade:", NC_Grade,"                            |")
+print("| Drivers License:", NC_driversLicense,"               |")
+print("| Email:", NC_Email,"      |")
+print("-----------------------------------------")
+print()
+
+print("-----------------------------------------")
+print("| Name:", MB_Name ,"                     |")
+print("| Age:", MB_Age,"                           |")
+print("| Grade:", MB_Grade,"                            |")
+print("| Drivers License:", MB_driversLicense,"               |")
+print("| Email:", MB_Email,"      |")
+print("-----------------------------------------")
+print()
+
+print("-----------------------------------------")
+print("| Name:", MD_Name ,"                  |")
+print("| Age:", MD_Age,"                           |")
+print("| Grade:", MD_Grade,"                            |")
+print("| Drivers License:", MD_driversLicense,"               |")
+print("| Email:", MD_Email,"      |")
+print("-----------------------------------------")
+print()
+
+print("-----------------------------------------")
+print("| Name:", JR_Name ,"                  |")
+print("| Age:", JR_Age,"                           |")
+print("| Grade:", JR_Grade,"                            |")
+print("| Drivers License:", JR_driversLicense,"               |")
+print("| Email:", JR_Email,"      |")
+print("-----------------------------------------")
+print()
